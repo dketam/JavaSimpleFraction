5dfdb5a95027ba4d789f365dc66a439824f815f8
KETAM DJILLALi <ketam.djalal@gmail.com>
2017-10-02 14:55:32 +0200

    ajout des fonctions de Fraction et le test de main

--- JavaSimpleFraction/src/Fraction.java
+++ JavaSimpleFraction/src/Fraction.java
@@ -1,4 +1,21 @@
 
 public class Fraction {
-
+private double numer,denume;
+	
+	public Fraction(double num, double denum)
+	{
+		this.numer = num;
+		this.denume = denum;
+	}
+	
+	public Fraction()
+	{
+		this(0, 0);
+	}
+	
+	@Override
+	public String toString()
+	{
+		return Double.valueOf(numer).toString() + " " + Double.valueOf(numer).toString();
+	}
 }
--- JavaSimpleFraction/src/main.java
+++ JavaSimpleFraction/src/main.java
@@ -1,4 +1,7 @@
 
 public class main {
-
+public	static void main(String args[]){
+Fraction f=new Fraction(2,3);
+System.out.println(f.toString());
+}
 }
